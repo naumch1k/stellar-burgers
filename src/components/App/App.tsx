8a050235e18a1e5f23b39ef6556d5fb21860025f
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-07-14 19:31:19 -0500

    feat: replaced mock data with a fetch request

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,24 +1,46 @@
+import { useState, useEffect } from 'react'
 import { Page } from '../Page'
 import { Header } from '../Header'
 import { BurgerIngredients } from '../BurgerIngredients'
 import { BurgerConstructor } from '../BurgerConstructor'
-
-import ingredientsData from '../../mocks/ingredients-data.json'
+import { MAIN_API } from '../../shared/constants/main-api'
 
 
 export const App = () => {
+  const [ingredientsData, setIngredientsData] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const getIngredients = async () => {
+      try {
+        const response = await fetch(MAIN_API)
+        const data = await response.json()
+        setIngredientsData(data)
+        setIsLoading(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getIngredients()
+  }, [])
+
   return (
     <Page>
       <Page.Header>
         <Header />
       </Page.Header>
       <Page.Content>
-        <BurgerIngredients
-          ingredients={ingredientsData}
-        />
-        <BurgerConstructor
-          ingredients={ingredientsData}
-        />
+      {!isLoading &&
+        <>
+          <BurgerIngredients
+            ingredients={ingredientsData}
+          />
+          <BurgerConstructor
+            ingredients={ingredientsData}
+          />
+        </>
+      }
       </Page.Content>
     </Page>
   )
---
+++ src/shared/constants/main-api.ts
@@ -0,0 +1,1 @@
+export const MAIN_API = 'https://my-json-server.typicode.com/naumch1k/stellar-burgers/ingredients/'
