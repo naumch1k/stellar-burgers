822ce3801cd87351a98f27d0f871d3e43a69049c
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-04 17:56:51 -0500

    refactor: set up routing, moved initial screen into the pages folder

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,43 +1,18 @@
-import { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { Page } from '../page'
-import { Header } from '../header'
-import { BurgerIngredients } from '../burger-ingredients'
-import { BurgerConstructor } from '../burger-constructor'
-import { Loader } from '../loader'
-import { OrderDetailsContext } from '../../contexts/order-details-context'
-import store from '../../store/store'
-import { fetchIngredients } from '../../store/ingredients/operations'
-import { IRootState } from '../../store/store'
+import { Routes, Route } from 'react-router-dom'
+import { Page } from '../../components/page'
+import { Header } from '../../components/header'
+import Main from '../../pages/main/main'
 
 export const App = () => {
-  const [orderDetails, setOrderDetails] = useState({})
-  const loadingStatus = useSelector((state: IRootState) => state.ingredients.status)
-
-  useEffect(() => {
-    store.dispatch(fetchIngredients())
-  }, [])
-
-  if (loadingStatus === 'loading') {
-    return (
-      <Loader/>
-    )
-  }
-
   return (
     <Page>
       <Page.Header>
         <Header/>
       </Page.Header>
       <Page.Content>
-        <OrderDetailsContext.Provider value={{ orderDetails, setOrderDetails }}>
-          <DndProvider backend={HTML5Backend}>
-            <BurgerIngredients/>
-            <BurgerConstructor/>
-          </DndProvider>
-        </OrderDetailsContext.Provider>
+        <Routes>
+          <Route path='/' element={<Main/>}/>
+        </Routes>
       </Page.Content>
     </Page>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App/>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
