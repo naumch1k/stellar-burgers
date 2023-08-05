81f0d465991eefbaf9ea0093376b25a72fc461cc
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-04 23:05:03 -0500

    feat: laid out reset-password page

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,6 +5,7 @@ import Main from '../../pages/main/main'
 import Login from '../../pages/login/login'
 import Register from '../../pages/register/register'
 import ForgotPassword from '../../pages/forgot-password/forgot-password'
+import ResetPassword from '../../pages/reset-password/reset-password'
 
 export const App = () => {
   return (
@@ -18,6 +19,8 @@ export const App = () => {
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
           <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route path='/forgot-password' element={<ForgotPassword/>}/>
+          <Route path='/reset-password' element={<ResetPassword/>}/>
         </Routes>
       </Page.Content>
     </Page>
---
+++ src/pages/reset-password/reset-password.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import {
+  PasswordInput,
+  Input,
+  Button,
+} from '@ya.praktikum/react-developer-burger-ui-components'
+import AuthPageLayout from '../../components/auth-page-layout'
+import { AuthPageTitle } from '../../components/auth-page-title'
+import { AuthForm } from '../../components/auth-form'
+import { AuthLink } from '../../components/auth-link'
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState('')
+  const [code, setCode] = useState('')
+
+  return (
+    <AuthPageLayout>
+      <AuthPageTitle title='Create a new passsword'/>
+      <AuthForm>
+        <PasswordInput
+          value={password}
+          placeholder='Enter new password'
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+        <Input
+          type='text'
+          value={code}
+          placeholder='Enter verification code'
+          onChange={e => setCode(e.target.value)}
+          required
+        />
+        <Button
+          htmlType='submit'
+          type='primary'
+          size='medium'
+        >
+          Save new password
+        </Button>
+      </AuthForm>
+      <AuthPageLayout.Links>
+        <AuthLink
+          leadInText='Remembered the password?'
+          linkText='Log in here'
+          to='/login'
+        />
+      </AuthPageLayout.Links>
+    </AuthPageLayout>
+  )
+}
+
+export default ResetPassword
