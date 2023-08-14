346eacd4294cbb50d8c6be711d2d62984ca6ead1
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-08-13 19:15:00 -0500

    fix: switched to react's HashRouter to ensure proper rendering of routes on GitHub pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './index.css'
@@ -13,9 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App/>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 )
