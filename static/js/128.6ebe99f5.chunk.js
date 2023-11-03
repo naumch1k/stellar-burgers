b0fc3e576c7afaa432340a550724ef93f049b5fe
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-11-02 22:05:26 -0500

    Updates

---
+++ static/js/128.6ebe99f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstellar_burgers=self.webpackChunkstellar_burgers||[]).push([[128],{3128:function(r,e,n){n.r(e),n.d(e,{default:function(){return l}});var s="Main_root__slkAg",u=n(184),l=function(){return(0,u.jsx)("div",{className:s,children:(0,u.jsx)("h1",{children:"Main"})})}}}]);
+//# sourceMappingURL=128.6ebe99f5.chunk.js.map
