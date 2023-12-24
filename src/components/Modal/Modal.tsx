e471eff713679a3fabe5700dcd7bbf89de9392b7
Irina Naumchik <irina.naumchik2012@yandex.ru>
2023-12-24 14:52:14 -0600

    feat: utilized React Portal for Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import { MouseEvent, ReactNode } from 'react'
+import { createPortal } from 'react-dom'
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components'
 import styles from './Modal.module.css'
 
@@ -14,21 +15,24 @@ export const Modal = ({
   onClose,
   onBackdropClick,
   children,
-}: IModalProps) => (
-  <div
-    className={`${styles.root} ${isOpen ? `${styles.isOpen}` : ''}`}
-    onClick={onBackdropClick}
-  >
-    <div className={`${styles.container} pt-25 pr-10 pb-25 pl-10`}>
-      {children}
-      <button
-        className={styles.closeButton}
-        type='button'
-        aria-label='Close modal'
-        onClick={onClose}
-      >
-        <CloseIcon type="primary"/>
-      </button>
-    </div>
-  </div>
-)
+}: IModalProps) => {
+  return createPortal(
+    <div
+      className={`${styles.root} ${isOpen ? `${styles.isOpen}` : ''}`}
+      onClick={onBackdropClick}
+    >
+      <div className={`${styles.container} pt-25 pr-10 pb-25 pl-10`}>
+        {children}
+        <button
+          className={styles.closeButton}
+          type='button'
+          aria-label='Close modal'
+          onClick={onClose}
+        >
+          <CloseIcon type='primary'/>
+        </button>
+      </div>
+    </div>,
+    document.querySelector('.portal')!
+  )
+}
