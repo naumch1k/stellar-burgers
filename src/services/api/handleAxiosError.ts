bee2915f9cc5f56b7671bd647f497002959e9cd4
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-05 10:47:12 -0600

    feat: refactored handleAxiosError for enhanced error handling and user-friendly messages

--- src/services/api/handleAxiosError.ts
+++ src/services/api/handleAxiosError.ts
@@ -2,15 +2,14 @@ import axios, { AxiosError } from 'axios'
 import { IApiFailureResponse } from './types'
 
 export const handleAxiosError = (error: unknown) => {
-  if (axios.isAxiosError(error)) {
-    const axiosError = error as AxiosError<IApiFailureResponse>
+  if (!axios.isAxiosError(error))
+    return { message: 'An unknown error occurred' }
 
-    if (axiosError.response && axiosError.response.data && axiosError.response.data.message) {
-      return { message: axiosError.response.data.message }
-    }
+  const axiosError = error as AxiosError<IApiFailureResponse>
+  const errorMessage = axiosError.response?.data?.message
 
-    return { message: error.message }
-  }
+  if (errorMessage === 'Token is invalid')
+    return { message: 'Your session has timed out. Please log in again.' }
 
-  return { message: 'An unknown error occurred' }
+  return { message: errorMessage || axiosError.message || 'An unknown error occurred' }
 }
