3ded6f33c9fb2899c0fa510af7237ad4569a649b
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-24 17:05:38 -0600

    refactor: enhanced BurgerSvg with className prop, converted atributes' names to camelCase

--- src/images/icons/burger.svg.tsx
+++ src/images/icons/burger.svg.tsx
@@ -1,30 +1,34 @@
-export const BurgerSvg = () => {
+interface IBurgerSvg {
+  className: string;
+}
+
+export const BurgerSvg = ({ className }: IBurgerSvg) => {
   return (
-    <svg width='78' height='78' viewBox='0 0 50 50' fill='none' aria-hidden='true' xmlns='http://www.w3.org/2000/svg'>
-      <g stroke='#fff' stroke-linecap='round' stroke-linejoin='round' filter='url(#a)'>
+    <svg className={className} viewBox='0 0 50 50' fill='none' aria-hidden='true' xmlns='http://www.w3.org/2000/svg'>
+      <g stroke='#fff' strokeLinecap='round' strokeLinejoin='round' filter='url(#a)'>
         <path d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
       </g>
-      <g stroke='#fff' stroke-linecap='round' stroke-linejoin='round' filter='url(#b)'>
+      <g stroke='#fff' strokeLinecap='round' strokeLinejoin='round' filter='url(#b)'>
         <path d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
       </g>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='.5' d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='.5' d='M40.51 37.63c.5-.61.05-1.49-.74-1.5-10.1-.17-18.56.02-29.01.65-.8.05-1.2.95-.68 1.54 3.67 4.1 9 6.68 14.92 6.68 6.26 0 11.84-2.87 15.51-7.37ZM7.64 15.06c-.39.68.19 1.47.97 1.38 11.56-1.4 20.09-1.8 32.18-1.07.79.05 1.31-.78.88-1.44a19.99 19.99 0 0 0-34.03 1.13Z'/>
       <g filter='url(#c)'>
-        <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
+        <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
       </g>
       <g filter='url(#d)'>
-        <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
+        <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
       </g>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='.5' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='.5' d='M44.86 27.47a1.86 1.86 0 0 0-1.74-2.04 251.3 251.3 0 0 0-36.23-.5c-1.06.05-1.9.93-1.8 1.99.14 1.5.45 2.95.9 4.33.26.8 1.04 1.3 1.88 1.26 12.51-.43 22.19-.46 34.33-.14.83.03 1.61-.47 1.87-1.27.37-1.17.64-2.38.79-3.63Z'/>
       <g filter='url(#e)'>
-        <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
+        <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
       </g>
       <g filter='url(#f)'>
-        <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
+        <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
       </g>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
-      <path stroke='#fff' stroke-linecap='round' stroke-linejoin='round' stroke-width='.5' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
+      <path stroke='#fff' strokeLinecap='round' strokeLinejoin='round' strokeWidth='.5' d='M5 20.5c2.67-2 5.33-2 8 0s5.33 2 8 0 5.33-2 8 0 5.33 2 8 0 5.33-2 8 0'/>
       <g stroke='#fff' filter='url(#g)'>
         <path d='M18.58 39.32c.5.25.73.76.54 1.13-.2.36-.75.45-1.23.2-.5-.26-.73-.77-.54-1.13.2-.37.74-.46 1.23-.2ZM33.45 12.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM30.97 39.26c.53-.14 1.05.06 1.16.46.1.4-.24.84-.78.99-.53.14-1.05-.07-1.16-.47-.1-.4.24-.84.78-.98ZM23.5 11c-.83 0-1.5.45-1.5 1s.67 1 1.5 1 1.5-.45 1.5-1-.67-1-1.5-1ZM27.45 9.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM25 41c.55 0 1 .34 1 .75s-.45.75-1 .75-1-.34-1-.75.45-.75 1-.75ZM17.26 11.47c.73-.39 1.12-1.1.86-1.58-.26-.5-1.06-.58-1.8-.2-.73.4-1.11 1.1-.86 1.59.26.49 1.06.57 1.8.19Z' />
       </g>
@@ -32,45 +36,45 @@ export const BurgerSvg = () => {
         <path d='M18.58 39.32c.5.25.73.76.54 1.13-.2.36-.75.45-1.23.2-.5-.26-.73-.77-.54-1.13.2-.37.74-.46 1.23-.2ZM33.45 12.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM30.97 39.26c.53-.14 1.05.06 1.16.46.1.4-.24.84-.78.99-.53.14-1.05-.07-1.16-.47-.1-.4.24-.84.78-.98ZM23.5 11c-.83 0-1.5.45-1.5 1s.67 1 1.5 1 1.5-.45 1.5-1-.67-1-1.5-1ZM27.45 9.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM25 41c.55 0 1 .34 1 .75s-.45.75-1 .75-1-.34-1-.75.45-.75 1-.75ZM17.26 11.47c.73-.39 1.12-1.1.86-1.58-.26-.5-1.06-.58-1.8-.2-.73.4-1.11 1.1-.86 1.59.26.49 1.06.57 1.8.19Z' />
       </g>
       <path stroke='#fff' d='M18.58 39.32c.5.25.73.76.54 1.13-.2.36-.75.45-1.23.2-.5-.26-.73-.77-.54-1.13.2-.37.74-.46 1.23-.2ZM33.45 12.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM30.97 39.26c.53-.14 1.05.06 1.16.46.1.4-.24.84-.78.99-.53.14-1.05-.07-1.16-.47-.1-.4.24-.84.78-.98ZM23.5 11c-.83 0-1.5.45-1.5 1s.67 1 1.5 1 1.5-.45 1.5-1-.67-1-1.5-1ZM27.45 9.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM25 41c.55 0 1 .34 1 .75s-.45.75-1 .75-1-.34-1-.75.45-.75 1-.75ZM17.26 11.47c.73-.39 1.12-1.1.86-1.58-.26-.5-1.06-.58-1.8-.2-.73.4-1.11 1.1-.86 1.59.26.49 1.06.57 1.8.19Z'/>
-      <path stroke='#fff' stroke-width='.5' d='M18.58 39.32c.5.25.73.76.54 1.13-.2.36-.75.45-1.23.2-.5-.26-.73-.77-.54-1.13.2-.37.74-.46 1.23-.2ZM33.45 12.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM30.97 39.26c.53-.14 1.05.06 1.16.46.1.4-.24.84-.78.99-.53.14-1.05-.07-1.16-.47-.1-.4.24-.84.78-.98ZM23.5 11c-.83 0-1.5.45-1.5 1s.67 1 1.5 1 1.5-.45 1.5-1-.67-1-1.5-1ZM27.45 9.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM25 41c.55 0 1 .34 1 .75s-.45.75-1 .75-1-.34-1-.75.45-.75 1-.75ZM17.26 11.47c.73-.39 1.12-1.1.86-1.58-.26-.5-1.06-.58-1.8-.2-.73.4-1.11 1.1-.86 1.59.26.49 1.06.57 1.8.19Z'/>
+      <path stroke='#fff' strokeWidth='.5' d='M18.58 39.32c.5.25.73.76.54 1.13-.2.36-.75.45-1.23.2-.5-.26-.73-.77-.54-1.13.2-.37.74-.46 1.23-.2ZM33.45 12.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM30.97 39.26c.53-.14 1.05.06 1.16.46.1.4-.24.84-.78.99-.53.14-1.05-.07-1.16-.47-.1-.4.24-.84.78-.98ZM23.5 11c-.83 0-1.5.45-1.5 1s.67 1 1.5 1 1.5-.45 1.5-1-.67-1-1.5-1ZM27.45 9.32c.8.21 1.56-.04 1.7-.58.15-.53-.38-1.14-1.18-1.35-.8-.22-1.57.04-1.71.58-.14.53.39 1.14 1.19 1.35ZM25 41c.55 0 1 .34 1 .75s-.45.75-1 .75-1-.34-1-.75.45-.75 1-.75ZM17.26 11.47c.73-.39 1.12-1.1.86-1.58-.26-.5-1.06-.58-1.8-.2-.73.4-1.11 1.1-.86 1.59.26.49 1.06.57 1.8.19Z'/>
       <defs>
-        <filter id='a' width='43.32' height='49' x='3.01' y='.5' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='a' width='43.32' height='49' x='3.01' y='.5' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='b' width='43.32' height='49' x='3.01' y='.5' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='b' width='43.32' height='49' x='3.01' y='.5' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='c' width='48.79' height='17.01' x='.58' y='20' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='c' width='48.79' height='17.01' x='.58' y='20' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='d' width='48.79' height='17.01' x='.58' y='20' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='d' width='48.79' height='17.01' x='.58' y='20' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='e' width='49' height='12' x='.5' y='14.5' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+        <filter id='e' width='49' height='12' x='.5' y='14.5' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix' />
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='f' width='49' height='12' x='.5' y='14.5' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix' />
+        <filter id='f' width='49' height='12' x='.5' y='14.5' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix' />
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='g' width='28.79' height='44.19' x='10.89' y='2.81' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='g' width='28.79' height='44.19' x='10.89' y='2.81' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
-        <filter id='h' width='28.79' height='44.19' x='10.89' y='2.81' color-interpolation-filters='sRGB' filterUnits='userSpaceOnUse'>
-          <feFlood flood-opacity='0' result='BackgroundImageFix'/>
+        <filter id='h' width='28.79' height='44.19' x='10.89' y='2.81' colorInterpolationFilters='sRGB' filterUnits='userSpaceOnUse'>
+          <feFlood floodOpacity='0' result='BackgroundImageFix'/>
           <feBlend in='SourceGraphic' in2='BackgroundImageFix' result='shape'/>
           <feGaussianBlur result='effect1_foregroundBlur_219_71' stdDeviation='2'/>
         </filter>
