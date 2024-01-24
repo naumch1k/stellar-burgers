bc7ff5047e9ebbc444019a1baa15b9e9b4218714
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-24 17:06:41 -0600

    feat: added initial useWindowWidth custom hook

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+function useWindowWidth() {
+  // TODO: add debouncer
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+
+  const updateWindowWidth = () => setWindowWidth(window.innerWidth)
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowWidth)
+
+    return () => window.removeEventListener('resize', updateWindowWidth)
+  }, [])
+
+  return { windowWidth }
+}
+
+export default useWindowWidth
