efeee8babc53ff35b23ffc4b4063677d95290a08
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-24 17:02:31 -0600

    refactor: enhanced Link UI component with className prop

--- src/components/ui/Link/Link.tsx
+++ src/components/ui/Link/Link.tsx
@@ -3,6 +3,7 @@ import { Link as RouterLink } from 'react-router-dom'
 import styles from './Link.module.css'
 
 interface ILinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  className?: string;
   href: string,
   isOutsideLink?: boolean;
   view?: 'primary' | 'secondary';
@@ -12,13 +13,14 @@ interface ILinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 export const Link = (props: ILinkProps) => {
   const {
     children,
+    className,
     href,
     isOutsideLink = false,
     view = 'primary',
     size,
   } = props
 
-  const classes = `${styles.root} ${styles[view]} ${size ? styles[size] : ''}`
+  const classes = `${className} ${styles.root} ${styles[view]} ${size ? styles[size] : ''}`
 
   return (
     !isOutsideLink ? (
