9c9347fc2258187264b55ef64b6cf007a1282a61
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-01-25 09:12:57 -0600

    cleanup: removed unused helper function

--- src/shared/helpers/capitalizeFirstLetter.ts
+++
@@ -1,6 +0,0 @@
-export const capitalizeFirstLetter = (string: string) => {
-  if (string.length === 0) return string
-
-  const lowercaseString = string.toLowerCase()
-  return lowercaseString.charAt(0).toUpperCase() + lowercaseString.slice(1)
-}
