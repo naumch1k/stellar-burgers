84096bc2f73790f60055656261d09519cff9bda5
Irina Naumchik <irina.naumchik2012@yandex.ru>
2025-11-18 21:48:01 -0600

    cleanup: removed obsolete WebSocket code from order feature

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -1,35 +1,14 @@
 import { useParams } from 'react-router'
-import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import { OrderDetails } from 'components/OrderDetails'
 import { ErrorMessage } from 'components/ErrorMessage'
-import { Loader } from 'components/Loader'
-import { IRootState, useAppDispatch } from 'store/store'
-import {
-  selectWebSocketState,
-  connect,
-  disconnect,
-} from 'store/webSocket/webSocket.slice'
+import { IRootState } from 'store/store'
 import { selectOrderById } from 'store/orders/orders.selectors'
-import { WS_BASE_URL, ACCESS_TOKEN_KEY } from 'shared/constants'
 import styles from './Order.module.css'
 
 const Order = () => {
   const { id } = useParams()
-  const dispatch = useAppDispatch()
-  const { isConnecting, isFetching } = useSelector(selectWebSocketState)
   const order = useSelector((state: IRootState) => selectOrderById(state, id!))
-  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY)?.split('Bearer ')[1]
-
-  useEffect(() => {
-    dispatch(connect(`${WS_BASE_URL}?token=${accessToken}`))
-
-    return () => {
-      dispatch(disconnect())
-    }
-  }, [dispatch, accessToken])
-
-  if (isConnecting || isFetching) return <Loader/>
 
   return (
     <div className={styles.root}>
